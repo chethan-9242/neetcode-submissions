08c3d37fa87efed1badcb038afd29b4b3fadd1b4
Chethan.A <chethanakkashali123@gmail.com>
2026-09-21 20:18:29 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int cost=0;
+        int maxi=0;
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            mini=min(mini,prices[i]);
+            cost=prices[i]-mini;
+            maxi=max(maxi,cost);
+        }
+        return maxi;
+    }
+};
---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int n=heights.size();
+        int left=0;
+        int right=n-1;
+        int maxi=0;
+        while(left<right)
+        {
+            int area=(right-left)*min(heights[left],heights[right]);
+            maxi=max(maxi,area);
+            if(heights[left]<heights[right])
+            {
+                left++;
+            }
+            else 
+            {
+                right--;
+            }
+        }
+    return maxi;
+    }
+};
